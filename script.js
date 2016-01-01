b349aa4957eff2e12130985bef4ee18a8b0ce0d1
jonucode <jonalmazan@ucode.com>
2016-01-01 07:37:16 +0000

    Edited descriptions

--- script.js
+++ script.js
@@ -42,56 +42,53 @@ $(window).bind('resize', function() {
 //////////////////// control hide and show navigation bar
 
 function expandNav(paraID) {
+    if($('#divExpandNav #' + paraID).is(":visible")) {
+        $('#divExpandNav #' + paraID).slideUp('fast');
+    }
+    if($('#divExpandNav #' + paraID).is(":hidden")) {
+        $('#divExpandNav #' + paraID).slideDown('fast');
+    }
     $('#divExpandNav p').not($('#divExpandNav #' + paraID)).slideUp('fast');
-    if($('#divExpandNav #' + paraID).is(":visible")) $('#divExpandNav #' + paraID).slideUp('fast');
-    if($('#divExpandNav #' + paraID).is(":hidden")) $('#divExpandNav #' + paraID).slideDown('fast');
 }
 /////////////////////////////// popup video examples
 //////////////////////////////
 // popup examples
-$( document ).on( "pagecreate", function() {
+$(document).on("pagecreate", function() {
     // The window width and height are decreased by 30 to take the tolerance of 15 pixels at each side into account
-    function scale( width, height, padding, border ) {
-        var scrWidth = $( window ).width() - 30,
-            scrHeight = $( window ).height() - 30,
+    function scale(width, height, padding, border) {
+        var scrWidth = $(window).width() - 30,
+            scrHeight = $(window).height() - 30,
             ifrPadding = 2 * padding,
             ifrBorder = 2 * border,
             ifrWidth = width + ifrPadding + ifrBorder,
             ifrHeight = height + ifrPadding + ifrBorder,
             h, w;
-        if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
+        if(ifrWidth < scrWidth && ifrHeight < scrHeight) {
             w = ifrWidth;
             h = ifrHeight;
-        } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
+        } else if((ifrWidth / scrWidth) > (ifrHeight / scrHeight)) {
             w = scrWidth;
-            h = ( scrWidth / ifrWidth ) * ifrHeight;
+            h = (scrWidth / ifrWidth) * ifrHeight;
         } else {
             h = scrHeight;
-            w = ( scrHeight / ifrHeight ) * ifrWidth;
+            w = (scrHeight / ifrHeight) * ifrWidth;
         }
         return {
-            'width': w - ( ifrPadding + ifrBorder ),
-            'height': h - ( ifrPadding + ifrBorder )
+            'width': w - (ifrPadding + ifrBorder),
+            'height': h - (ifrPadding + ifrBorder)
         };
     };
-    $( ".ui-popup iframe" )
-        .attr( "width", 0 )
-        .attr( "height", "auto" );
-    $( "#popupVideo" ).on({
+    $(".ui-popup iframe").attr("width", 0).attr("height", "auto");
+    $("#popupVideo").on({
         popupbeforeposition: function() {
             // call our custom function scale() to get the width and height
-            var size = scale( 497, 298, 15, 1 ),
+            var size = scale(497, 298, 15, 1),
                 w = size.width,
                 h = size.height;
-            $( "#popupVideo iframe" )
-                .attr( "width", w )
-                .attr( "height", h );
+            $("#popupVideo iframe").attr("width", w).attr("height", h);
         },
         popupafterclose: function() {
-            $( "#popupVideo iframe" )
-                .attr( "width", 0 )
-                .attr( "height", 0 );
+            $("#popupVideo iframe").attr("width", 0).attr("height", 0);
         }
     });
 });
-///////////////////////
